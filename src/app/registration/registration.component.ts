876aa3c304edaa4eebd9683b06659b9ea886c8cb
Jishnu Gopalan K P <55798251+jishnugopalan@users.noreply.github.com>
2022-04-05 23:44:37 +0530

    second commit

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,11 +1,48 @@
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { AppComponent } from '../app.component';
+
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
+  registration=new FormGroup({
+    username:new FormControl('',[
+      Validators.required
+    ]),
+    email:new FormControl('',[
+      Validators.required,
+      Validators.email
+    ]),
+    phone:new FormControl('',[
+      Validators.required,
+      Validators.minLength(10),
+      Validators.maxLength(10),
+      
+    ]),
+    department:new FormControl('',[
+      Validators.required
+    ])
+    
+  })
+  get department(){
+    return this.registration.get('department')
+
+  }
+  get username(){
+    return this.registration.get('username')
+
+  }
+  get email(){
+    return this.registration.get('email')
+
+  }
+  get phone(){
+    return this.registration.get('phone')
+
+  }
 
   constructor() { }
 
